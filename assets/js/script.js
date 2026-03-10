9eb82f81e02b1bf66fc4b0e5c9288f9a1208d176
bondarenko15 <tonissstar058@gmail.com>
2026-03-10 16:54:44 +0100

    main .5v

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 AOS.init();
+
+
+const searchToggle = document.querySelector('.search-active');
+const headerSearch = document.querySelector('.header-search');
+const searchInput = headerSearch.querySelector('input');
+
+searchToggle.addEventListener('click', () => {
+    const isOpen = headerSearch.classList.toggle('open');
+
+    if (!isOpen) {
+        searchInput.value = '';
+    }
+});
+
+//Header scroll 
+
+let lastScroll = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > lastScroll) {
+        header.classList.add('hide');
+    } else {
+        header.classList.remove('hide');
+    }
+
+    lastScroll = currentScroll;
+});
